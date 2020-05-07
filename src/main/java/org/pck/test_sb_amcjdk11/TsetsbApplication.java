56a894a68963cfd39b868063d48f3e07227818f4
coldflow <dev.coldflow@gmail.com>
2020-05-07 22:55:12 +0900

    initial commit

---
+++ src/main/java/org/pck/test_sb_amcjdk11/TsetsbApplication.java
@@ -0,0 +1,12 @@
+package org.pck.test_sb_amcjdk11;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetsbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TsetsbApplication.class, args);
+    }
+}
